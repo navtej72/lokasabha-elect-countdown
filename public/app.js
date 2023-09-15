f320d93b4ff63e221ddadb490b2ff7943be4b7f5
navtej bhatti <navbhtt0@gmail.com>
2023-09-15 12:42:25 +0530

    complete files

---
+++ public/app.js
@@ -0,0 +1,27 @@
+function updateCountdown() {
+    const now = new Date();
+    const timeDifference = targetDate - now;
+
+    if (timeDifference <= 0) {
+        clearInterval(countdownInterval);
+        // You can add any action you want when the countdown reaches 0 here
+        document.getElementById('countdown').textContent = 'Time is up!';
+    } else {
+        const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+        document.getElementById('days').textContent = days;
+        document.getElementById('hours').textContent = hours;
+        document.getElementById('minutes').textContent = minutes;
+        document.getElementById('seconds').textContent = seconds;
+    }
+}
+
+const targetDate = new Date('2024-03-30T00:00:00');
+
+let countdownInterval = setInterval(updateCountdown, 1000);
+
+// Initial update
+updateCountdown();
